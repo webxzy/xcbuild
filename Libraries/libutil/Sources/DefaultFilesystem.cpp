2cdc386c442adf284c2b5c1892b70ac7cb31c568
Grant Paul <git@grantpaul.com>
2016-10-13 18:12:42 +0000

    WIP Build on Windows with MSVC.
    Does not build yet.

--- Libraries/libutil/Sources/DefaultFilesystem.cpp
+++ Libraries/libutil/Sources/DefaultFilesystem.cpp
@@ -16,55 +16,100 @@
 #include <cstring>
 #include <cerrno>
 
+#if _WIN32
+#include <Windows.h>
+#else
 #include <unistd.h>
 #include <libgen.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#endif
 
 using libutil::DefaultFilesystem;
+
+#if _WIN32
+using WideString = std::vector<std::remove_const<std::remove_pointer<LPCWSTR>::type>::type>;
+
+static WideString
+StringToWideString(std::string const &str)
+{
+	int size = MultiByteToWideChar(CP_UTF8, 0, str.data(), (int)str.size(), NULL, 0);
+	WideString wide = WideString(size);
+	MultiByteToWideChar(CP_UTF8, 0, str.data(), (int)str.size(), wide.data(), size);
+	return wide;
+}
+#endif
 
 bool DefaultFilesystem::
 exists(std::string const &path) const
 {
+#if _WIN32
+	WideString wide = StringToWideString(path);
+	DWORD attributes = GetFileAttributesW(wide.data());
+	return (attributes != INVALID_FILE_ATTRIBUTES);
+#else
     return ::access(path.c_str(), F_OK) == 0;
+#endif
 }
 
 bool DefaultFilesystem::
 isDirectory(std::string const &path) const
 {
-    struct stat st;
-    if (::stat(path.c_str(), &st) < 0)
-        return false;
-    else
-        return S_ISDIR(st.st_mode);
+#if _WIN32
+	return false;
+#else
+	struct stat st;
+	if (::stat(path.c_str(), &st) < 0) {
+		return false;
+	} else {
+		return S_ISDIR(st.st_mode);
+	}
+#endif
 }
 
 bool DefaultFilesystem::
 isSymbolicLink(std::string const &path) const
 {
-    struct stat st;
-    if (::lstat(path.c_str(), &st) < 0)
-        return false;
-    else
-        return S_ISLNK(st.st_mode);
+#if _WIN32
+	return false;
+#else
+	struct stat st;
+	if (::lstat(path.c_str(), &st) < 0) {
+		return false;
+	} else {
+		return S_ISLNK(st.st_mode);
+	}
+#endif
 }
 
 bool DefaultFilesystem::
 isReadable(std::string const &path) const
 {
+#if _WIN32
+	return false;
+#else
     return ::access(path.c_str(), R_OK) == 0;
+#endif
 }
 
 bool DefaultFilesystem::
 isWritable(std::string const &path) const
 {
+#if _WIN32
+	return false;
+#else
     return ::access(path.c_str(), W_OK) == 0;
+#endif
 }
 
 bool DefaultFilesystem::
 isExecutable(std::string const &path) const
 {
+#if _WIN32
+	return false;
+#else
     return ::access(path.c_str(), X_OK) == 0;
+#endif
 }
 
 bool DefaultFilesystem::
@@ -103,9 +148,13 @@ createDirectory(std::string const &path)
         }
         current += component;
 
+#if _WIN32
+		return false;
+#else
         if (::mkdir(current.c_str(), S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) != 0 && errno != EEXIST) {
             return false;
         }
+#endif
     }
 
     return true;
@@ -131,7 +180,7 @@ read(std::vector<uint8_t> *contents, std::string const &path, size_t offset, ext
     }
 
     if (length) {
-        if (offset + *length > size) {
+        if (offset + *length > static_cast<size_t>(size)) {
             std::fclose(fp);
             return false;
         }
@@ -181,6 +230,9 @@ write(std::vector<uint8_t> const &contents, std::string const &path)
 ext::optional<std::string> DefaultFilesystem::
 readSymbolicLink(std::string const &path) const
 {
+#if _WIN32
+	return ext::nullopt;
+#else
     char buffer[PATH_MAX];
     ssize_t len = ::readlink(path.c_str(), buffer, sizeof(buffer) - 1);
     if (len == -1) {
@@ -189,38 +241,51 @@ readSymbolicLink(std::string const &path) const
 
     buffer[len] = '\0';
     return std::string(buffer);
+#endif
 }
 
 bool DefaultFilesystem::
 writeSymbolicLink(std::string const &target, std::string const &path)
 {
+#if _WIN32
+	return false;
+#else
     if (symlink(target.c_str(), path.c_str())) {
         return false;
     }
 
     return true;
+#endif
 }
 
 bool DefaultFilesystem::
 removeFile(std::string const &path)
 {
+#if _WIN32
+	return false;
+#else
     if (::unlink(path.c_str()) < 0) {
         if (::unlink(path.c_str()) < 0) {
             return false;
         }
     }
     return true;
+#endif
 }
 
 std::string DefaultFilesystem::
 resolvePath(std::string const &path) const
 {
+#if _WIN32
+	return false;
+#else
     char realPath[PATH_MAX + 1];
     if (::realpath(path.c_str(), realPath) == nullptr) {
         return std::string();
     } else {
         return realPath;
     }
+#endif
 }
 
 bool DefaultFilesystem::
@@ -228,6 +293,9 @@ enumerateDirectory(
     std::string const &path,
     std::function<void(std::string const &)> const &cb) const
 {
+#if _WIN32
+	return false;
+#else
     DIR *dp = opendir(path.c_str());
     if (dp == NULL) {
         return false;
@@ -242,5 +310,6 @@ enumerateDirectory(
 
     closedir(dp);
     return true;
+#endif
 }
 
